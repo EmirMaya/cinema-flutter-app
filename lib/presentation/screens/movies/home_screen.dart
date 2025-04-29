80fd982a94217c961ed37b7dc3de098b40c1c746
Emir Maya <emirmayaok@gmail.com>
2025-04-28 21:17:30 -0300

    Custom loader file

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -43,6 +43,8 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     final upcomingMovies = ref.watch(upcomingMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
 
+    return FullScreenLoader();
+
     if (moviesSlide.isEmpty) return const CircularProgressIndicator();
 
     return CustomScrollView(
